bc159e8d747216f86b349fe1fb0e6acc8c936b38
Hacker Gaming <69993535+Sandi950@users.noreply.github.com>
2020-10-16 22:06:05 +0700

    PDKT

---
+++ PDKT.sh
@@ -0,0 +1,71 @@
+
+clear
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+sleep 1
+echo "Loading...." | lolcat
+sleep 7
+clear
+sleep 1
+echo "Welcome The Tools" | lolcat
+sleep 3
+clear
+figlet BUCIN | lolcat
+echo "========================================================" | lolcat
+echo "Kenalan Dulu Kali" | lolcat
+sleep 1
+echo
+echo -n "Siapa Nama Lu : " ;read dd
+sleep 1
+echo -n "Nama Orang Yang Mau Di Chat : " ;read nn
+echo
+echo "Sabar Bro Lagi Loading...." | lolcat
+sleep 10
+clear
+figlet PDKT | lolcat
+echo "=============================================================" | lolcat
+echo "($nn) : P"
+echo -n "($dd) : ";read ab
+echo "($nn) : Lagi Apa?"
+echo -n "($dd) : ";read ab
+echo "($nn) : Oh"
+echo -n "($dd) : ";read ab
+echo "($nn) : Udh Makan?"
+echo -n "($dd) : ";read ab
+echo "($nn) : Oh"
+echo -n "($dd) : ";read ab
+echo "($nn) : Kamu Sehat?"
+echo -n "($dd) : ";read ab
+echo "($nn) : Oh"
+echo -n "($dd) : ";read ab
+echo "($nn) : Aku Mau Nanya"
+echo -n "($dd) : ";read ab
+echo "($nn) : Kamu Mau Gak Jadi Pacar Aku?"
+echo -n "($dd) : ";read ab
+echo "($nn) : Ya Udh Sayang Aku Mau Of Dulu"
+echo -n "($dd) : ";read ab
+echo "($nn) : Jangan Lupa Istirahat"
+echo -n "($dd) : ";read ab
+echo "($nn) : Dah Sayang"
+echo -n "($dd) : ";read ab
+echo "($nn) : Aku Sayang Kamu"
+echo -n "($dd) : ";read ab
+sleep 1
+echo "===============================================================" | lolcat
+echo "Inget Ya Bro Itu Bukan Dia Yang Chat" | lolcat
+echo "Ini Tuh Cuma Script, Jadi Jangan Di Anggap Serius" | lolcat
+echo "Ya Udh Gw Mau Off Dulu Semoga Bermanfaat" | lolcat
+sleep 5
+clear
+figlet BYE | lolcat
+exit
